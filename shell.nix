ac225fefb661b994cff5890eb4b402098e06055b
Justus K <justus.k@protonmail.com>
2020-11-30 22:51:32 +0100

    chore: add shell.nix and default.nix

---
+++ shell.nix
@@ -0,0 +1,4 @@
+(import (fetchTarball {
+  url = "https://github.com/edolstra/flake-compat/archive/master.zip";
+  sha256 = "0x2jn3vrawwv9xp15674wjz9pixwjyj3j771izayl962zziivbx2";
+}) { src = ./.; }).shellNix
